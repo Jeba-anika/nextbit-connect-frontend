6dc21975864369783f5b7f6e436c5a8e9dcca5ba
Jeba Anika <anikanur98@gmail.com>
2023-11-03 01:48:26 +0600

    code refactor

--- src/components/Forms/ACDepartmentField.tsx
+++ src/components/Forms/ACDepartmentField.tsx
@@ -1,32 +1,32 @@
-import { useAcademicDepartmentsQuery } from "@/redux/api/academic/departmentApi";
-import FormSelectField, { SelectOptions } from "./FormSelectField";
+// import { useAcademicDepartmentsQuery } from "@/redux/api/academic/departmentApi";
+// import FormSelectField, { SelectOptions } from "./FormSelectField";
 
-type ACDepartmentFieldProps = {
-  name: string;
-  label?: string;
-};
+// type ACDepartmentFieldProps = {
+//   name: string;
+//   label?: string;
+// };
 
-const ACDepartmentField = ({ name, label }: ACDepartmentFieldProps) => {
-  const { data, isLoading } = useAcademicDepartmentsQuery({
-    limit: 100,
-    page: 1,
-  });
-  const academicDepartments = data?.academicDepartments;
-  const acDepartmentOptions = academicDepartments?.map((acDepartment: any) => {
-    console.log(acDepartment?.id);
-    return {
-      label: acDepartment?.title,
-      value: acDepartment?.id,
-    };
-  });
+// const ACDepartmentField = ({ name, label }: ACDepartmentFieldProps) => {
+//   const { data, isLoading } = useAcademicDepartmentsQuery({
+//     limit: 100,
+//     page: 1,
+//   });
+//   const academicDepartments = data?.academicDepartments;
+//   const acDepartmentOptions = academicDepartments?.map((acDepartment: any) => {
+//     console.log(acDepartment?.id);
+//     return {
+//       label: acDepartment?.title,
+//       value: acDepartment?.id,
+//     };
+//   });
 
-  return (
-    <FormSelectField
-      name={name}
-      label={label}
-      options={acDepartmentOptions as SelectOptions[]}
-    />
-  );
-};
+//   return (
+//     <FormSelectField
+//       name={name}
+//       label={label}
+//       options={acDepartmentOptions as SelectOptions[]}
+//     />
+//   );
+// };
 
-export default ACDepartmentField;
+// export default ACDepartmentField;
--- src/components/Forms/ACFacultyField.tsx
+++ src/components/Forms/ACFacultyField.tsx
@@ -1,31 +1,31 @@
-import { useAcademicFacultiesQuery } from "@/redux/api/academic/facultyApi";
-import FormSelectField, { SelectOptions } from "./FormSelectField";
+// import { useAcademicFacultiesQuery } from "@/redux/api/academic/facultyApi";
+// import FormSelectField, { SelectOptions } from "./FormSelectField";
 
-type ACFacultyFieldProps = {
-  name: string;
-  label: string;
-};
+// type ACFacultyFieldProps = {
+//   name: string;
+//   label: string;
+// };
 
-const ACFacultyField = ({ name, label }: ACFacultyFieldProps) => {
-  const { data, isLoading } = useAcademicFacultiesQuery({
-    limit: 100,
-    page: 1,
-  });
-  const academicFaculties = data?.academicFaculties;
-  const acFacultyOptions = academicFaculties?.map((acFaculty) => {
-    return {
-      label: acFaculty?.title,
-      value: acFaculty?.id,
-    };
-  });
+// const ACFacultyField = ({ name, label }: ACFacultyFieldProps) => {
+//   const { data, isLoading } = useAcademicFacultiesQuery({
+//     limit: 100,
+//     page: 1,
+//   });
+//   const academicFaculties = data?.academicFaculties;
+//   const acFacultyOptions = academicFaculties?.map((acFaculty) => {
+//     return {
+//       label: acFaculty?.title,
+//       value: acFaculty?.id,
+//     };
+//   });
 
-  return (
-    <FormSelectField
-      name={name}
-      label={label}
-      options={acFacultyOptions as SelectOptions[]}
-    />
-  );
-};
+//   return (
+//     <FormSelectField
+//       name={name}
+//       label={label}
+//       options={acFacultyOptions as SelectOptions[]}
+//     />
+//   );
+// };
 
-export default ACFacultyField;
+// export default ACFacultyField;
